9429f63b98f6fbcf4f7cccc133a4036cd404fc1e
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-08-01 16:18:52 +0300

    Add mobile support

--- templates/footer.php
+++ templates/footer.php
@@ -1,3 +1,5 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.1.1/min/dropzone.min.js"></script>
 <script>
     Dropzone.options.fileUpload = {
